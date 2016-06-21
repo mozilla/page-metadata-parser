f57d2d86fe9dff7772f07656c6250424bae3fea7
Jared Kerim <jared.l.kerim@gmail.com>
2016-06-21 16:10:08 +0000

    Linting for tests

--- tests/test_metadata.js
+++ tests/test_metadata.js
@@ -7,15 +7,15 @@ const {getMetadata} = require('../parser.js');
 
 
 describe('Metadata tests', function() {
-    it('extracts metadata', function () {
-        const document = jsdom.jsdom(`
-          <html>
-            <head>
-              <title>Test Page</title>
-            </head>
-          </html>
-        `);
-        const metadata = getMetadata(document);
-        assert.equal(metadata.title, 'Test Page');
-    });
+  it('extracts metadata', function() {
+      const document = jsdom.jsdom(`
+        <html>
+          <head>
+            <title>Test Page</title>
+          </head>
+        </html>
+      `);
+      const metadata = getMetadata(document);
+      assert.equal(metadata.title, 'Test Page');
+  });
 });
