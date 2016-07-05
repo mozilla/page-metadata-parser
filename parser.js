3e0a01569249edff6accfff0631de8543e036b24
k88hudson <k88hudson@gmail.com>
2016-07-05 17:22:40 -0400

    Make parser extendable

--- parser.js
+++ parser.js
@@ -1,95 +1,40 @@
 const {dom, rule, ruleset} = require('fathom-web');
+const DEFAULT_RULESET = require('./lib/default-ruleset');
 
+const MetadataParser = {
+  metadataRules: Object.assign({}, DEFAULT_RULESET),
+  buildRuleset(name, rules) {
+    const reversedRules = Array.from(rules).reverse();
+    const builtRuleset = ruleset(...reversedRules.map(([query, handler], order) => rule(
+      dom(query),
+      node => [{
+        score: order,
+        flavor: name,
+        notes: handler(node),
+      }]
+    )));
 
-function buildRuleset(name, rules) {
-  const reversedRules = Array.from(rules).reverse();
-  const builtRuleset = ruleset(...reversedRules.map(([query, handler], order) => rule(
-    dom(query),
-    node => [{
-      score: order,
-      flavor: name,
-      notes: handler(node),
-    }]
-  )));
-
-  return doc => {
-    const kb = builtRuleset.score(doc);
-    const maxNode = kb.max(name);
-    if (maxNode) {
-      const value = maxNode.flavors.get(name);
-      if (value) {
-        return value.trim();
+    return doc => {
+      const kb = builtRuleset.score(doc);
+      const maxNode = kb.max(name);
+      if (maxNode) {
+        const value = maxNode.flavors.get(name);
+        if (value) {
+          return value.trim();
+        }
       }
-    }
-  };
-}
+    };
+  },
+  getMetadata(doc) {
+    const metadata = {};
 
+    Object.keys(MetadataParser.metadataRules).forEach(metadataKey => {
+      const metadataRule = MetadataParser.buildRuleset(metadataKey, MetadataParser.metadataRules[metadataKey]);
+      metadata[metadataKey] = metadataRule(doc);
+    });
 
-const titleRules = buildRuleset('title', [
-  ['meta[property="og:title"]', node => node.element.content],
-  ['meta[property="twitter:title"]', node => node.element.content],
-  ['meta[name="hdl"]', node => node.element.content],
-  ['title', node => node.element.text],
-]);
-
-const canonicalUrlRules = buildRuleset('url', [
-  ['meta[property="og:url"]', node => node.element.content],
-  ['link[rel="canonical"]', node => node.element.href],
-]);
-
-
-const iconRules = buildRuleset('icon', [
-  ['link[rel="apple-touch-icon"]', node => node.element.href],
-  ['link[rel="apple-touch-icon-precomposed"]', node => node.element.href],
-  ['link[rel="icon"]', node => node.element.href],
-  ['link[rel="fluid-icon"]', node => node.element.href],
-  ['link[rel="shortcut icon"]', node => node.element.href],
-  ['link[rel="Shortcut Icon"]', node => node.element.href],
-  ['link[rel="mask-icon"]', node => node.element.href],
-]);
-
-const imageRules = buildRuleset('image', [
-  ['meta[property="og:image"]', node => node.element.content],
-  ['meta[property="twitter:image"]', node => node.element.content],
-  ['meta[name="thumbnail"]', node => node.element.content],
-  ['img', node => node.element.src],
-]);
-
-const descriptionRules = buildRuleset('description', [
-  ['meta[property="og:description"]', node => node.element.content],
-  ['meta[name="description"]', node => node.element.content],
-]);
-
-const typeRules = buildRuleset('type', [
-  ['meta[property="og:type"]', node => node.element.content],
-]);
-
-
-const metadataRules = {
-  description: descriptionRules,
-  icon_url: iconRules,
-  image_url: imageRules,
-  title: titleRules,
-  type: typeRules,
-  url: canonicalUrlRules
+    return metadata;
+  }
 };
 
-
-function getMetadata(doc, rules) {
-  const metadata = {};
-  const ruleSet = rules || metadataRules;
-
-  Object.keys(ruleSet).map(metadataKey => {
-    const metadataRule = ruleSet[metadataKey];
-    metadata[metadataKey] = typeof metadataRule === 'function' ?
-      metadataRule(doc) :
-      getMetadata(doc, metadataRule);
-  });
-
-  return metadata;
-}
-
-module.exports = {
-  metadataRules,
-  getMetadata
-};
+module.exports = MetadataParser;
